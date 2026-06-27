8b82874cb2ff7156f3972787fbeccd92ddb713da
blofar486-pixel <blofar486@gmail.com>
2026-06-27 11:08:58 +0500

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,1 +1,20 @@
 
+export default function handler(req,res){
+
+const MASTER_KEY="RIZII-2026-PRO";
+
+if(req.method==="POST"){
+
+const {key}=req.body;
+
+if(key===MASTER_KEY){
+return res.status(200).json({success:true});
+}
+
+return res.status(401).json({success:false});
+
+}
+
+res.status(405).end();
+
+}
